82f7ff14f729bad347019bfaaf6596e317cafd89
Wlezley Lishack <wlezley.lishack@gmail.com>
2025-02-26 08:05:51 +0100

    WG API login to get an access_token

--- src/actions/increment-counter.ts
+++ src/actions/increment-counter.ts
@@ -3,38 +3,38 @@ import * as fs from 'fs/promises';
 
 import WotAccountInfo from "WotAccountInfo";
 import WgApiError from "WgApiError";
+import { SettingsSchema } from "../plugin";
 
 /**
  * Settings for {@link IncrementCounter}.
  */
-type CounterSettings = {
+type PluginSettings = {
 	count?: number;
-	player_id: number;
-	wg_api_id: number;
+	account_id: number;
+	application_id: string;
 	filename: string;
 };
 
 @action({ UUID: "com.wlezley-lishack.wot-tracker.increment" })
-export class IncrementCounter extends SingletonAction<CounterSettings> {
+export class IncrementCounter extends SingletonAction<PluginSettings> {
 
-	override onWillAppear(ev: WillAppearEvent<CounterSettings>): void | Promise<void> {
-		return ev.action.setTitle(`${ev.payload.settings.count ?? 0}`);
+	override onWillAppear(ev: WillAppearEvent<PluginSettings>): void | Promise<void> {
+		return ev.action.setTitle(`${ev.payload.settings.count ?? 'WoT'}`);
 	}
 
-	override onDidReceiveSettings(ev: DidReceiveSettingsEvent<CounterSettings>): void {
+	override onDidReceiveSettings(ev: DidReceiveSettingsEvent<PluginSettings>): void {
 		// Handle the settings changing in the property inspector (UI).
 		streamDeck.logger.info("SETTINGS CHANGED: ", ev.payload.settings);
 	}
 
-	override async onKeyDown(ev: KeyDownEvent<CounterSettings>): Promise<void> {
-		// streamDeck.system.openUrl("https://elgato.com");
-
+	override async onKeyDown(ev: KeyDownEvent<PluginSettings>): Promise<void> {
 		const { settings } = ev.payload;
 
-		// fs.writeFile(settings.filename, "aaaaaa");
+		const globalSettings = await streamDeck.settings.getGlobalSettings() as SettingsSchema;
 
-		if (settings.player_id && settings.wg_api_id) {
-			const API_URL = `https://api.worldoftanks.eu/wot/account/info/?application_id=${settings.wg_api_id}&account_id=${settings.player_id}`;
+
+		if (settings.application_id && globalSettings.account_id && globalSettings.access_token) {
+			const API_URL = `https://api.worldoftanks.eu/wot/account/info/?application_id=${settings.application_id}&account_id=${globalSettings.account_id}&access_token=${globalSettings.access_token}`;
 
 			fetch(API_URL)
 				.then(response => response.json())
@@ -43,11 +43,14 @@ export class IncrementCounter extends SingletonAction<CounterSettings> {
 					// streamDeck.logger.debug(`API DATA`, apidata);
 
 					if (apidata.status == "ok") {
-						if (apidata.data[settings.player_id]) {
-							streamDeck.logger.info(`G_RATING`, apidata.data[settings.player_id].global_rating);
-							streamDeck.logger.info(`CLAN`, apidata.data[settings.player_id].clan_id);
+						if (apidata.data[globalSettings.account_id]) {
+							streamDeck.logger.info(`G_RATING`, apidata.data[globalSettings.account_id].global_rating);
+							streamDeck.logger.info(`CLAN`, apidata.data[globalSettings.account_id].clan_id);
+							streamDeck.logger.info(`CREDITS`, apidata.data[globalSettings.account_id].private.credits);
 
-							fs.writeFile(settings.filename, apidata.data[settings.player_id].clan_id.toString());
+							if (settings.filename) {
+								fs.writeFile(settings.filename, apidata.data[globalSettings.account_id].private.credits.toString());
+							}
 						} else {
 							throw "Player not found";
 						}
@@ -57,7 +60,7 @@ export class IncrementCounter extends SingletonAction<CounterSettings> {
 				})
 				.catch(error => streamDeck.logger.error("WG API ERROR: ", error));
 		} else {
-			streamDeck.logger.error("Neni to vyplněný!");
+			streamDeck.logger.error("Missing parameters: application_id OR account_id OR access_token");
 		}
 	}
 }
